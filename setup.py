53db00edb384eecdd7c8368d52385b4dc6584cd6
TkskKurumi <zafkielkurumi@gmail.com>
2022-01-24 13:29:00 +0800

    better search algorithms

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="meow-memo",
-    version="0.0.5",
+    version="0.0.6",
     description="Quick memo",
     author="TkskKurumi",
     maintainer="TkskKurumi",
